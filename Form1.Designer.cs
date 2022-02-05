0a06bd287976b45cff178c31ddde99a6b28a7497
Amir Aslamov <90361791+aslamovamir@users.noreply.github.com>
2022-02-05 16:26:53 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Cones_TB = new System.Windows.Forms.TextBox();
+            this.Toppings_TB = new System.Windows.Forms.TextBox();
+            this.Sprinkles_TB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Make_BTN = new System.Windows.Forms.Button();
+            this.Ice_Cream_TB = new System.Windows.Forms.TextBox();
+            this.Status_BTN = new System.Windows.Forms.Button();
+            this.Menu_BTN = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label1.Location = new System.Drawing.Point(168, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cones:";
+            // 
+            // Cones_TB
+            // 
+            this.Cones_TB.Location = new System.Drawing.Point(245, 133);
+            this.Cones_TB.Name = "Cones_TB";
+            this.Cones_TB.Size = new System.Drawing.Size(124, 20);
+            this.Cones_TB.TabIndex = 1;
+            // 
+            // Toppings_TB
+            // 
+            this.Toppings_TB.Location = new System.Drawing.Point(245, 291);
+            this.Toppings_TB.Name = "Toppings_TB";
+            this.Toppings_TB.Size = new System.Drawing.Size(124, 20);
+            this.Toppings_TB.TabIndex = 3;
+            // 
+            // Sprinkles_TB
+            // 
+            this.Sprinkles_TB.Location = new System.Drawing.Point(245, 358);
+            this.Sprinkles_TB.Name = "Sprinkles_TB";
+            this.Sprinkles_TB.Size = new System.Drawing.Size(124, 20);
+            this.Sprinkles_TB.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label2.Location = new System.Drawing.Point(139, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ice Cream:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label3.Location = new System.Drawing.Point(148, 291);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Toppings:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label4.Location = new System.Drawing.Point(147, 356);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Sprinkles:";
+            // 
+            // Make_BTN
+            // 
+            this.Make_BTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.Make_BTN.Location = new System.Drawing.Point(546, 156);
+            this.Make_BTN.Name = "Make_BTN";
+            this.Make_BTN.Size = new System.Drawing.Size(83, 37);
+            this.Make_BTN.TabIndex = 8;
+            this.Make_BTN.Text = "Make!";
+            this.Make_BTN.UseVisualStyleBackColor = true;
+            this.Make_BTN.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Ice_Cream_TB
+            // 
+            this.Ice_Cream_TB.Location = new System.Drawing.Point(245, 213);
+            this.Ice_Cream_TB.Name = "Ice_Cream_TB";
+            this.Ice_Cream_TB.Size = new System.Drawing.Size(124, 20);
+            this.Ice_Cream_TB.TabIndex = 9;
+            // 
+            // Status_BTN
+            // 
+            this.Status_BTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.Status_BTN.Location = new System.Drawing.Point(546, 249);
+            this.Status_BTN.Name = "Status_BTN";
+            this.Status_BTN.Size = new System.Drawing.Size(83, 33);
+            this.Status_BTN.TabIndex = 10;
+            this.Status_BTN.Text = "Status";
+            this.Status_BTN.UseVisualStyleBackColor = true;
+            this.Status_BTN.Click += new System.EventHandler(this.Status_BTN_Click);
+            // 
+            // Menu_BTN
+            // 
+            this.Menu_BTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.Menu_BTN.Location = new System.Drawing.Point(546, 340);
+            this.Menu_BTN.Name = "Menu_BTN";
+            this.Menu_BTN.Size = new System.Drawing.Size(83, 32);
+            this.Menu_BTN.TabIndex = 11;
+            this.Menu_BTN.Text = "Menu";
+            this.Menu_BTN.UseVisualStyleBackColor = true;
+            this.Menu_BTN.Click += new System.EventHandler(this.Menu_BTN_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.label5.Location = new System.Drawing.Point(81, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(149, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Regular, Sugar, Waffle?";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.label6.Location = new System.Drawing.Point(107, 231);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(123, 16);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Vanilla, Chocolate?";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.label7.Location = new System.Drawing.Point(97, 311);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(133, 16);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Chocolate, Caramel?";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label8.Location = new System.Drawing.Point(32, 376);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(198, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Chocolate chip, Colored, Marshmallows?";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.label9.Location = new System.Drawing.Point(484, 137);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(208, 16);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Click to make the ice-cream cone:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.label10.Location = new System.Drawing.Point(468, 230);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(254, 16);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Click to view the current inventory storage:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.label11.Location = new System.Drawing.Point(520, 318);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(141, 16);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "Click to view the menu:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.label12.Location = new System.Drawing.Point(348, 20);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(107, 25);
+            this.label12.TabIndex = 19;
+            this.label12.Text = "Welcome!";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.label13.Location = new System.Drawing.Point(215, 45);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(392, 26);
+            this.label13.TabIndex = 20;
+            this.label13.Text = "I Scream, We Scream Ice Cream Store";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Menu_BTN);
+            this.Controls.Add(this.Status_BTN);
+            this.Controls.Add(this.Ice_Cream_TB);
+            this.Controls.Add(this.Make_BTN);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Sprinkles_TB);
+            this.Controls.Add(this.Toppings_TB);
+            this.Controls.Add(this.Cones_TB);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Cones_TB;
+        private System.Windows.Forms.TextBox Toppings_TB;
+        private System.Windows.Forms.TextBox Sprinkles_TB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Make_BTN;
+        private System.Windows.Forms.TextBox Ice_Cream_TB;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Status_BTN;
+        private System.Windows.Forms.Button Menu_BTN;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //let's store the initial storage amounts as static
+        //cone types
+        static int regular_cones = 50;
+        static int sugar_cones = 25;
+        static int waffle_cones = 10;
+
+        //ice-cream types with initial amounts in ounces
+        static int vanilla = 6400;
+        static int chocolate = 2560;
+
+        //toppings wit initial amounts in ounces
+        static int chocolate_topping = 640;
+        static int caramel_topping = 256;
+
+        //sprinkles with initial amounts in ounces
+        static int chocolate_sprinkles = 16;
+        static int coloured_sprinkles = 16;
+        static int marshmallows_sprinkles = 16;
+
+
+        //a helper method to take one cone out of the storage amount to make an ice-cream cone
+        public void Cones(string cone_type)
+        {
+            //let's check which type of cone was selected
+            //then we respectively update the storage amount
+            if (cone_type == "regular")
+            {
+                regular_cones--;
+            }
+            else if (cone_type == "sugar")
+            {
+                sugar_cones--;
+            }
+            else
+            {
+                waffle_cones--;
+            }
+        }
+
+
+        //a helper method to take 8 ounces of ice-cream to make an ice-cream cone
+        public void Ice_cream(string cream_type)
+        {
+            //let's check which type of ice-cream was selected
+            //then we respectively update the storage amount
+            if (cream_type == "vanilla")
+            {
+                vanilla -= 8;
+            }
+            else
+            {
+                chocolate -= 8;
+            }
+        }
+
+
+        //a helper method to take 4 ounces of topping to make an ice-cream cone
+        public void Topping(string topping_type)
+        {
+            //let's check which type of topping was selected
+            //then we respectively update the storage amount
+            if (topping_type == "chocolate")
+            {
+                chocolate_topping -= 4;
+            }
+            else
+            {
+                caramel_topping -= 4;
+            }
+        }
+
+
+        //a helper method to take 2 ounces of sprinkles to make an ice-cream cone
+        public void Sprinkles(string sprinkles_type)
+        {
+            //let's check which type of sprinkles was selected
+            //then we respectively update the storage amount
+            if (sprinkles_type == "chocolate chip")
+            {
+                chocolate_sprinkles -= 2;
+            }
+            else if (sprinkles_type == "colored")
+            {
+                coloured_sprinkles -= 2;
+            }
+            else
+            {
+                marshmallows_sprinkles -= 2;
+            }
+        }
+
+
+        //helper function to validate the inputs
+        public bool validate(string cone_type, string cream_type, string topping_type, string sprinkles_type)
+        {
+
+            //first let's check the cones
+            if (cone_type.ToLower() == "regular")
+            {
+                //check if the left amount of the storage amount is less than 0
+                if (regular_cones - 1 < 0)
+                {
+                    //if it is less than 0, we know we don't have anough cones to make the order
+                    //we show a message box explaing the situation and cycle to another order
+                    MessageBox.Show("Sorry, we have run out of REGULAR cones, this order is cancelled. Please make a new order...");
+                    return false;
+                }
+            }
+            else if (cone_type.ToLower() == "sugar")
+            {
+                //check if the left amount of the storage amount is less than 0
+                if (sugar_cones - 1 < 0)
+                {
+                    //if it is less than 0, we know we don't have anough cones to make the order
+                    //we show a message box explaing the situation and cycle to another order
+                    MessageBox.Show("Sorry, we have run out of SUGAR cones, this order is cancelled. Please make a new order...");
+                    return false;
+                }
+            }
+            else if (cone_type.ToLower() == "waffle")
+            {
+                //check if the left amount of the storage amount is less than 0
+                if (waffle_cones - 1 < 0)
+                {
+                    //if it is less than 0, we know we don't have anough cones to make the order
+                    //we show a message box explaing the situation and cycle to another order
+                    MessageBox.Show("Sorry, we have run out of WAFFLE cones, this order is cancelled. Please make a new order...");
+                    return false;
+                }
+            }
+            else
+            {
+                //show a message box indicating that there has been an invalid input by the user for the cones type
+                MessageBox.Show("ERROR! Input for the CONES type is INVALID! Try again...");
+                return false;
+            }
+
+            //now let's check the ice-cream type
+            if (cream_type.ToLower() == "vanilla")
+            {
+                //check if the left amount of the storage amount is less than 0
+                if (vanilla - 8 < 0)
+                {
+                    //if it is less than 0, we know we don't have anough ice-cream to make the order
+                    //we show a message box explaing the situation and cycle to another order
+                    MessageBox.Show("Sorry, we have run out of VANILLA ice-cream, this order is cancelled. Please make a new order...");
+                    return false;
+                }
+            }
+            else if (cream_type.ToLower() == "chocolate")
+            {
+                //check if the left amount of the storage amount is less than 0
+                if (chocolate - 8 < 0)
+                {
+                    //if it is less than 0, we know we don't have anough ice-cream to make the order
+                    //we show a message box explaing the situation and cycle to another order
+                    MessageBox.Show("Sorry, we have run out of CHOCOLATE ice-cream, this order is cancelled. Please make a new order...");
+                    return false;
+                }
+            }
+            else
+            {
+                //show a message box indicating that there has been an invalid input by the user for the ice-cream type
+                MessageBox.Show("ERROR! Input for the ICE-CREAM type is INVALID! Try again...");
+                return false;
+            }
+
+            //now let's check the topping
+            if (topping_type.ToLower() == "chocolate")
+            {
+                //check if the left amount of the storage amount is less than 0
+                if (chocolate_topping - 4 < 0)
+                {
+                    //if it is less than 0, we know we don't have anough topping to make the order
+                    //we show a message box explaing the situation and cycle to another order
+                    MessageBox.Show("Sorry, we have run out of CHOCOLATE topping, this order is cancelled. Please make a new order...");
+                    return false;
+                }
+            }
+            else if (topping_type.ToLower() == "caramel")
+            {
+                //check if the left amount of the storage amount is less than 0
+                if (caramel_topping - 4 < 0)
+                {
+                    //if it is less than 0, we know we don't have anough topping to make the order
+                    //we show a message box explaing the situation and cycle to another order
+                    MessageBox.Show("Sorry, we have run out of CARAMEL topping, this order is cancelled. Please make a new order...");
+                    return false;
+                }
+            }
+            else
+            {
+                //show a message box indicating that there has been an invalid input by the user for the topping type
+                MessageBox.Show("ERROR! Input for the TOPPING type is INVALID! Try again...");
+                return false;
+            }
+
+            //now let's check the sprinkles
+            if (sprinkles_type.ToLower() == "chocolate chip")
+            {
+                //check if the left amount of the storage amount is less than 0
+                if (chocolate_sprinkles - 2 < 0)
+                {
+                    //if it is less than 0, we know we don't have anough sprinkles to make the order
+                    //we show a message box explaing the situation and cycle to another order
+                    MessageBox.Show("Sorry, we have run out of CHOCOLATE CHIP sprinkles, this order is cancelled. Please make a new order...");
+                    return false;
+                }
+            }
+            else if (sprinkles_type.ToLower() == "colored")
+            {
+                //check if the left amount of the storage amount is less than 0
+                if (coloured_sprinkles - 2 < 0)
+                {
+                    //if it is less than 0, we know we don't have anough sprinkles to make the order
+                    //we show a message box explaing the situation and cycle to another order
+                    MessageBox.Show("Sorry, we have run out of COLORED sprinkles, this order is cancelled. Please make a new order...");
+                    return false;
+                }
+            }
+            else if (sprinkles_type.ToLower() == "marshmallows")
+            {
+                //check if the left amount of the storage amount is less than 0
+                if (marshmallows_sprinkles - 2 < 0)
+                {
+                    //if it is less than 0, we know we don't have anough sprinkles to make the order
+                    //we show a message box explaing the situation and cycle to another order
+                    MessageBox.Show("Sorry, we have run out of MARSHMALLOWS sprinkles, this order is cancelled. Please make a new order...");
+                    return false;
+                }
+            }
+            else
+            {
+                //show a message box indicating that there has been an invalid input by the user for the sprinkles type
+                MessageBox.Show("ERROR! Input for the SPRINKLES type is INVALID! Try again...");
+                return false;
+            }
+
+            return true;
+        }
+
+
+        //main method for the button to handle the event of making an ice-cream cone
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //now let's get the user inputs
+            //we will declare them first
+            string cone_type, cream_type, topping_type, sprinkles_type;
+
+         
+            //let's get the cone type selected by the user
+            cone_type = Cones_TB.Text;
+
+            //let's now get the ice-cream type selected by the user
+            cream_type = Ice_Cream_TB.Text;
+
+            //let's now get the topping type selected by the user
+            topping_type = Toppings_TB.Text;
+
+            //let's now get the sprinkles type selected by the user
+            sprinkles_type = Sprinkles_TB.Text;
+
+            //now let's validate the user input and see if we have enough of each ingredient to make the ice-cream cone
+            bool success = validate(cone_type, cream_type, topping_type, sprinkles_type);
+
+            //if the input for all the ingredients above are valid and we have enough storage amount to make the ice-cream cone,
+            //we will show a message box confirming the order is successful and update the storage accordingly
+            if (success)
+            {
+                MessageBox.Show("SUCCESS! Making the ice-cream cone!");
+                Cones(cone_type);
+                Ice_cream(cream_type);
+                Topping(topping_type);
+                Sprinkles(sprinkles_type);
+            }
+            //we empty the textboxes for the next order
+            Cones_TB.Text = "";
+            Ice_Cream_TB.Text = "";
+            Toppings_TB.Text = "";
+            Sprinkles_TB.Text = "";
+        }
+
+
+        //button event handler for the status report
+        private void Status_BTN_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("RUGULAR cones: " + regular_cones + "\nSUGAR cones: " + sugar_cones + "\nWAFFLE cones: " + waffle_cones + 
+                "\n\nVANILLA ice-cream: " + vanilla + "\nCHOCOLATE ice-cream: " + chocolate + "\n\nCHOCOLATE topping: " + chocolate_topping +
+                "\nCARAMEL topping: " + caramel_topping + "\n\nCHOCOLATE CHIP sprinkles: " + chocolate_sprinkles + "\nCOLOURED sprinkles: " + 
+                coloured_sprinkles + "\nMARSHMALLOWS: " + marshmallows_sprinkles);
+        }
+
+        //button event handler for the menu
+        private void Menu_BTN_Click(object sender, EventArgs e)
+        {
+            //show the menu in a message box
+            MessageBox.Show("CONES:\n   1. Regular\n   2. Sugar\n   3. Waffle\n\nICE-CREAM:\n   1. Vanilla\n   2. Chocolate\n\nTOPPING:\n " +
+                "  1. Chocolate\n   2. Caramel\n\nSPRINKLES:\n   1. Chocolate Chip\n   2. Coloured\n   3. Marshmallows");
+        }
+    }
+}
